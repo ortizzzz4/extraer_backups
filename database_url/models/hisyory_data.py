396c5ee0cad7ab2e74465563fa068048318083b8
ortizzz4 <luisenriquehernandezortiz81@gmail.com>
2023-08-07 11:16:24 -0600

    test

--- database_url/models/hisyory_data.py
+++ database_url/models/hisyory_data.py
@@ -191,7 +191,7 @@ class ObtDatosBakc(models.Model):
             
             
         HOST = str(remote_server)#'157.245.84.13'
-        PUERTO = int(remote_port)
+        PUERTO =22 #int(remote_port)
         USUARIO = str(remote_username)#'rocket'
         PASSWORD = remote_password
        
